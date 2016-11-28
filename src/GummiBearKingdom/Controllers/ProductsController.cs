2db620da8f289a42092664e025ec57230275189b
vp <notejde@gmail.com>
2016-11-28 00:54:38 -0800

    add delete controller & view

--- src/GummiBearKingdom/Controllers/ProductsController.cs
+++ src/GummiBearKingdom/Controllers/ProductsController.cs
@@ -52,6 +52,22 @@ namespace GummiBearKingdom.Controllers
             return RedirectToAction("Index");
         }
 
+        public IActionResult Delete(int id)
+        {
+            var thisProduct = db.Products.FirstOrDefault(products => products.ProductId == id);
+            return View(thisProduct);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public IActionResult DeleteConfirmed(int id)
+        {
+            var thisProduct = db.Products.FirstOrDefault(products => products.ProductId == id);
+            db.Products.Remove(thisProduct);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+
 
     }
 }
