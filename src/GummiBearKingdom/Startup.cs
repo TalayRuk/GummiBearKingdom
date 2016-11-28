c73cd26c44ce6b12448c36880a4a72acaabbd6a9
vp <notejde@gmail.com>
2016-11-27 23:11:48 -0800

    add update to .json for EF.tools

--- src/GummiBearKingdom/Startup.cs
+++ src/GummiBearKingdom/Startup.cs
@@ -7,11 +7,23 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using GummiBearKingdom.Models;
+using Microsoft.Extensions.Configuration;
+using Microsoft.EntityFrameworkCore;
 
 namespace GummiBearKingdom
 {
     public class Startup
     {
+        public IConfigurationRoot Configuration { get; set; }
+        public Startup(IHostingEnvironment env)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json");
+            Configuration = builder.Build();
+        }
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
@@ -20,6 +32,10 @@ namespace GummiBearKingdom
             //The ConfigureServices method is where we configure famework services to our application
 
             services.AddMvc();
+
+            services.AddEntityFramework()
+                .AddDbContext<GummiBearContext>(options =>
+                    options.UseSqlServer(Configuration["ConnectionStrings:DefaultConnection"]));
         }
 
         public void Configure(IApplicationBuilder app)
