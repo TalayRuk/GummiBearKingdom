616bdb4217f9f341967d7b582cb3dc700b5001b0
vp <notejde@gmail.com>
2016-11-27 23:21:53 -0800

    add update GummibearContext -forgot the brackets afterclass

--- src/GummiBearKingdom/Models/GummiBearContext.cs
+++ src/GummiBearKingdom/Models/GummiBearContext.cs
@@ -1,28 +1,27 @@
 ﻿using Microsoft.EntityFrameworkCore;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace GummiBearKingdom.Models
 {
     public class GummiBearContext : DbContext
-
-    //DbSet<Table> will be set as Table in the SQL database 
-    public DbSet<Category> Categories { get; set; }
-    public DbSet<Product> Products { get; set; }
-    public DbSet<Blog> Blogs { get; set; }
-    public DbSet<Post> Posts { get; set; }
-
-    protected override void OnConfiguring(DbContextOptionsBuilder options)
     {
-        options.UseSqlServer(@"Server=(localdb)\\MSSQLLocalDB;Database=GummiBearKingdom;Trusted_Connection=True");
+        //DbSet<Table> will be set as Table in the SQL database 
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Blog> Blogs { get; set; }
+        public DbSet<Post> Posts { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            options.UseSqlServer(@"Server=(localdb)\\MSSQLLocalDB;Database=GummiBearKingdom;Trusted_Connection=True");
+        }
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
     }
-    protected override void OnModelCreating(ModelBuilder builder)
-    {
-        base.OnModelCreating(builder);
-    }
-}
+
+    
+
 }
 
 //Next Configure our Startup class for the app to use Entity Framework
