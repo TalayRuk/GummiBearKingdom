85c48539398d367c1dbe45feba9f831d0d23e5c4
vp <notejde@gmail.com>
2016-11-30 11:36:17 -0800

    add ControllerTests Pass

---
+++ GummiBearKingdom.Tests/GummiBearKingdom.Tests/ControllerTests/ProductsControllerTest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using GummiBearKingdom.Controllers;
+using GummiBearKingdom.Models;
+using Xunit;
+
+namespace GummiBearKingdom.Tests.ControllerTests
+{
+    public class ProductsControllerTest
+    {
+        [Fact]
+        public void Get_ViewResult_Index_Test()
+        {
+            //Arrange 
+            ProductsController controller = new ProductsController();
+            //Act
+            var result = controller.Index();
+            //Assert
+            Assert.IsType<ViewResult>(result);
+        }
+    }
+}
