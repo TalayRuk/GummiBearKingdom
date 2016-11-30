4cc560ca28dd13615d9ad8ed015bfc9be73e1dda
vp <notejde@gmail.com>
2016-11-30 00:34:58 -0800

    add ProductTest

---
+++ GummiBearKingdom.Tests/GummiBearKingdom.Tests/ProductTest.cs
@@ -0,0 +1,22 @@
+﻿using GummiBearKingdom.Models;
+using Xunit;
+
+namespace GummiBearKingdom.Tests
+{
+    public class ProductTest
+    {
+        [Fact]
+        public void GetName()
+        {
+            //Arrange
+            var product = new Product();
+
+            //Act
+            var result = product.Name;
+
+            //Assert
+            Assert.Equal("Christmas", result);
+       
+        }
+    }
+}
