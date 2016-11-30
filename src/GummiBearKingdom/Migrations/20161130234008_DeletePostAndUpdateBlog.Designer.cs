95d36aaf11c5ef7d1b58b2dc4cd8da75a0f68954
vp <notejde@gmail.com>
2016-11-30 15:45:18 -0800

    Add DeletePost&UpdateBlogMigrations

--- src/GummiBearKingdom/Migrations/20161130234008_DeletePostAndUpdateBlog.Designer.cs
+++ src/GummiBearKingdom/Migrations/20161130234008_DeletePostAndUpdateBlog.Designer.cs
@@ -8,9 +8,10 @@ using GummiBearKingdom.Models;
 namespace GummiBearKingdom.Migrations
 {
     [DbContext(typeof(GummiBearContext))]
-    partial class GummiBearContextModelSnapshot : ModelSnapshot
+    [Migration("20161130234008_DeletePostAndUpdateBlog")]
+    partial class DeletePostAndUpdateBlog
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
             modelBuilder
                 .HasAnnotation("ProductVersion", "1.0.0-rtm-21431")
---
+++ src/GummiBearKingdom/Migrations/20161130234008_DeletePostAndUpdateBlog.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace GummiBearKingdom.Migrations
+{
+    public partial class DeletePostAndUpdateBlog : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Subject",
+                table: "Blogs");
+
+            migrationBuilder.DropTable(
+                name: "Posts");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Author",
+                table: "Blogs",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Text_body",
+                table: "Blogs",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Title",
+                table: "Blogs",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Author",
+                table: "Blogs");
+
+            migrationBuilder.DropColumn(
+                name: "Text_body",
+                table: "Blogs");
+
+            migrationBuilder.DropColumn(
+                name: "Title",
+                table: "Blogs");
+
+            migrationBuilder.CreateTable(
+                name: "Posts",
+                columns: table => new
+                {
+                    PostId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Author = table.Column<string>(nullable: true),
+                    BlogId = table.Column<int>(nullable: true),
+                    Text_body = table.Column<string>(nullable: true),
+                    Title = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Posts", x => x.PostId);
+                    table.ForeignKey(
+                        name: "FK_Posts_Blogs_BlogId",
+                        column: x => x.BlogId,
+                        principalTable: "Blogs",
+                        principalColumn: "BlogId",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.AddColumn<string>(
+                name: "Subject",
+                table: "Blogs",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Posts_BlogId",
+                table: "Posts",
+                column: "BlogId");
+        }
+    }
+}
