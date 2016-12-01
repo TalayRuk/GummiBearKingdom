04d7f06942d70a0c378e3b16edb01fefb00a0f2c
vp <notejde@gmail.com>
2016-11-30 19:47:09 -0800

    move README.md

---
+++ src/GummiBearKingdom/Migrations/20161201032240_AddChangeImageName.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace GummiBearKingdom.Migrations
+{
+    public partial class AddChangeImageName : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Image",
+                table: "Products");
+
+            migrationBuilder.AddColumn<string>(
+                name: "ImageURL",
+                table: "Products",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ImageURL",
+                table: "Products");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Image",
+                table: "Products",
+                nullable: true);
+        }
+    }
+}
