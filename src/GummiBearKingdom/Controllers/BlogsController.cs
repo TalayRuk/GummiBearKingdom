a11281a091c58386d93925fdb89c3a94a33d250f
vp <notejde@gmail.com>
2016-12-07 11:11:44 -0800

    add blog edit controller

--- src/GummiBearKingdom/Controllers/BlogsController.cs
+++ src/GummiBearKingdom/Controllers/BlogsController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using GummiBearKingdom.Models;
+using Microsoft.EntityFrameworkCore;
 
 // For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -34,6 +35,19 @@ namespace GummiBearKingdom.Controllers
         public IActionResult Create(Blog blog)
         {
             db.Blogs.Add(blog);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public IActionResult Edit(int id)
+        {
+            var thisBlog = db.Blogs.FirstOrDefault(b => b.BlogId == Id);
+            return View(thisBlog);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Blog blog)
+        {
+            db.Entry(blog).State = EntityState.Modified;
             db.SaveChanges();
             return RedirectToAction("Index");
         }
